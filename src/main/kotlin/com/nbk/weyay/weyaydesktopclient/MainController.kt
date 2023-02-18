f2e70dde24c18fe3df97a4870df1dc37549946eb
mohammad <mohammadmohammad@nbk.com>
2023-02-18 16:03:24 +0300

    init

--- src/main/kotlin/com/nbk/weyay/weyaydesktopclient/MainController.kt
+++ src/main/kotlin/com/nbk/weyay/weyaydesktopclient/MainController.kt
@@ -3,17 +3,22 @@
 package com.nbk.weyay.weyaydesktopclient
 
 import javafx.fxml.FXML
-import javafx.scene.control.Label
+import javafx.fxml.Initializable
+import javafx.geometry.Insets
+import javafx.scene.control.*
+import javafx.scene.layout.*
 import javafx.stage.FileChooser
 import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.consumeEach
 import kotlinx.coroutines.channels.produce
 import kotlinx.coroutines.javafx.JavaFx
 import java.io.File
+import java.net.URL
+import java.util.*
 import kotlin.coroutines.CoroutineContext
 
 
-class MainController: CoroutineScope {
+class MainController: CoroutineScope, Initializable {
 
     private var job = Job()
 
@@ -25,6 +30,46 @@ class MainController: CoroutineScope {
 
     @FXML
     private lateinit var statusLabel: Label
+
+    @FXML
+    private lateinit var tabPane: TabPane
+
+    override fun initialize(location: URL?, resources: ResourceBundle?) {
+        val tab = Tab("first").apply {
+            AnchorPane().apply {
+                VBox().apply {
+                    spacing = 5.0
+                    padding = Insets(5.0, 0.0, 0.0, 0.0)
+                    AnchorPane.setTopAnchor(this, 0.0)
+                    AnchorPane.setBottomAnchor(this, 0.0)
+                    AnchorPane.setRightAnchor(this, 0.0)
+                    AnchorPane.setLeftAnchor(this, 0.0)
+                    GridPane().apply {
+                        val colConstraints = ColumnConstraints(10.0, 60.0, 60.0, Priority.SOMETIMES, null, true)
+                        columnConstraints.setAll(colConstraints, colConstraints)
+                        rowConstraints.setAll(RowConstraints(10.0, 30.0, -1.0,  Priority.SOMETIMES, null, true))
+                        add(Button("Button").apply {
+                            maxWidth = 200.0
+                            isMnemonicParsing = false }, 0, 0)
+                        add(Button("Button").apply {
+                            maxWidth = 200.0
+                            isMnemonicParsing = false}, 1, 0)
+                    }.also { children.add(it) }
+                    TableView<String>().apply {
+                        VBox.setVgrow(this, Priority.ALWAYS)
+                        columns.addAll(
+                            TableColumn<String, String>().apply { text = "Column 1" },
+                            TableColumn<String, String>().apply { text = "Column 2" }
+                        )
+                        prefHeight = 200.0
+                        prefWidth = 200.0
+                    }.also { children.add(it) }
+                }.also { children.add(it) }
+            }.also { content = it }
+        }
+        tabPane.tabs.add(tab)
+        println("initialized")
+    }
 
     @FXML
     private fun onHelloButtonClick() {
